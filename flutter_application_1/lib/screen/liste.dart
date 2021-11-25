71ed80a5b877a66e2c27a8ce84a395bd3cda59fd
Jeremy barre <jeremy.barre@campus.academy>
2021-11-25 09:27:28 +0100

    Liste à effectuer

---
+++ flutter_application_1/lib/screen/liste.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class List extends StatelessWidget {
+  const List({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //titleTextStyle: const TextStyle(fontWeight: FontWeight.bold),
+        title: const Center(
+          child: const Text("Liste à effectuer"),
+        ),
+      ),
+      body: SizedBox(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.purple,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.pink,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
